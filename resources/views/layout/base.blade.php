722e910194a974824a2137208973f6129a92ea61
datalowe <datalowe@posteo.de>
2021-05-31 21:47:20 +0200

    split parts of base layout out to separate header/footer files

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -11,31 +11,12 @@
 
     </head>
     <body>
-        <header class="site-header">
-            <nav>
-                <ul>
-                    <li>
-                        <a href="{{ route('showLogin') }}">{{ __("Log in") }}</a>
-                    </li>
-                </ul>
-            </nav>
-            <div class="language-picker-wrapper">
-                <ul class="language-picker">
-                    <li>Language</li>
-                    <li><a href="{{ route('updateLocale', ['language' => 'ja']) }}">日本語</a></li>
-                    <li><a href="{{ route('updateLocale', ['language' => 'en']) }}">EN</a></li>
-                    <li><a href="{{ route('updateLocale', ['language' => 'sv']) }}">SV</a></li>
-                </ul>
-            </div>
-        </header>
-
+        @include('layout.header')
         <main>
             @yield('content')
         </main>
 
-        <footer>
-
-        </footer>
+        @include('layout.footer')
 
     </body>
 
