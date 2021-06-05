18a2bcfd5f14e12600db1040b54651eb896691a8
datalowe <datalowe@posteo.de>
2021-06-05 15:54:36 +0200

    add basic login/home route tests

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use Tests\TestCase;
+
+use App\Models\User;
+
+
+class AuthTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->userInfo = [
+            'name' => '中島 学',
+            'email' => 'akira.sasaki@foobar.net',
+            'password' => 'secretpass',
+        ];
+    }
+
+    /**
+     * Visiting root URL redirects non-logged in user
+     * to login screen.
+     */
+    public function testUnauthRedirect()
+    {
+        $response = $this->get('/');
+
+        $response
+            ->assertStatus(302)
+            ->assertRedirect(route('guest.showLogin'));
+    }
+
+    /**
+     * Visiting root URL as logged in user
+     * returns home view.
+     */
+    public function testAuthRedirect()
+    {
+        $user = User::factory()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->get('/');
+
+        $response
+            ->assertStatus(200)
+            ->assertViewIs('home');
+    }
+
+    /**
+     * POSTing to logout route as logged in user
+     * makes the session unauthenticated/logged out, and
+     * reroutes user to login screen.
+     */
+    public function testLogout()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        // ensure that acting as user really does qualify the session
+        // as being authenticated
+        $this->assertAuthenticated();
+
+        $response = $this->post(route('user.processLogout'));
+        $this->assertGuest();
+
+        $response
+            ->assertStatus(302)
+            ->assertRedirect(route('guest.showLogin'));
+    }
+
+    /**
+     * POSTing to login route with valid credentials 
+     * makes the session authenticated/logged in,
+     * and redirects user to home route.
+     */
+    public function testLogin()
+    {
+        $userInfo = $this->userInfo;
+
+        $user = User::create([
+            'name' => $userInfo['name'],
+            'email' => $userInfo['email'],
+            'password' => bcrypt($userInfo['password']),
+        ]);
+
+        $response = $this
+            ->post(route('guest.processLogin'), [
+                'email' => $userInfo['email'],
+                'password' => $userInfo['password']
+            ]);
+
+        $this->assertAuthenticated();
+
+        $response
+            ->assertStatus(302)
+            ->assertRedirect(route('user.home'));
+    }
+
+    /**
+     * POSTing to login route with invalid credentials 
+     * redirects user to root route (since there is no
+     * other route to send user 'back to').
+     */
+    public function testNonExistingUserLogin()
+    {
+        $userInfo = $this->userInfo;
+
+        $response = $this
+            ->post(route('guest.processLogin'), [
+                'email' => $userInfo['email'],
+                'password' => $userInfo['password']
+            ]);
+
+        $this->assertGuest();
+
+        $response
+            ->assertStatus(302)
+            ->assertRedirect('/');
+    }
+
+    /**
+     * POSTing to login route with valid username but
+     * invalid password six times locks the user account
+     * temporarily.
+     */
+    public function testWrongPassBlockLogin()
+    {
+        $userInfo = $this->userInfo;
+
+        $user = User::create([
+            'name' => $userInfo['name'],
+            'email' => $userInfo['email'],
+            'password' => bcrypt($userInfo['password']),
+        ]);
+
+        for ($i = 0; $i < 6; $i++) {
+            $response = $this->post(
+                route('guest.processLogin'), 
+                [
+                    'email' => $userInfo['email'],
+                    'password' => 'wrong_pass'
+                ]
+            );
+        }
+        
+        $dbUser = User::where('email', $userInfo['email'])->first();
+
+        $this->assertGuest();
+        $this->assertTrue($dbUser->locked_flag);
+        
+        $this->assertEquals($dbUser->error_count, 6);
+    }
+}
