9268221cb20f1d8d358c76d75c8c220237d5a4f8
datalowe <datalowe@posteo.de>
2021-06-13 19:17:14 +0200

    add footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,3 +1,5 @@
-<footer>
-
+<footer class="site-footer">
+    <div class="site-title-wrapper">
+        <a id="site-logo-small" class="site-logo site-logo-small" href="{{ route('newUsers') }}">LaraGram</a>
+    </div>
 </footer>
